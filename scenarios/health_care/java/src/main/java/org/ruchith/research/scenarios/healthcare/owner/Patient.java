92b915b47c1dd05046217f78a5343c8f1f490b74
Byungchan An <ban@purdue.edu>
2015-03-23 20:46:57 -0400

    java files for patient

---
+++ scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/owner/Patient.java
@@ -0,0 +1,37 @@
+package org.ruchith.research.scenarios.healthcare.owner;
+
+import org.ruchith.research.idm.idp.Configuration;
+import org.ruchith.research.idm.idp.IdentityManager;
+import org.ruchith.research.scenarios.healthcare.owner.db.Database;
+
+public class Patient extends IdentityManager {
+	// 01. As a identity manager, create claim definition
+	// 02. After receiving (req) value from the data consumer, 
+	// issue claim based on the claim definition
+	// 
+	
+	// one claim definition?
+	private Configuration config;
+	private Database db;
+	
+	public Patient(String configPath) throws Exception {
+		super(configPath); // Parent's constructor
+		this.config = Configuration.getInstance(configPath);
+		this.db = Database.getInstance(this.config.getDbHost(), this.config.getDbUser(), this.config.getDbPassword());
+	}
+	
+	public void addRequestPermissionQueue(String recordId, String name, int type) 
+		throws Exception {
+		db.addRequestPermission(recordId, name, type);
+	}
+	
+	public void updateRegisteration(String recordId, String name, boolean registered)
+		throws Exception {
+		db.updateRequestRegisterationStatus(recordId, name, registered);
+	}
+	
+	public void updateRecordPair(String recordId, String ownerName, String readName)
+		throws Exception {
+		db.groupRecordIdAndParams(recordId, ownerName, readName);
+	}
+}
---
+++ scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/owner/db/Database.java
@@ -0,0 +1,47 @@
+package org.ruchith.research.scenarios.healthcare.owner.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * 
+ * @author Byungchan An
+ *
+ */
+
+public class Database {
+	private static Database db;
+	private static Connection con;
+	private static final String DB_NAME = "idp";
+	
+	private Database(String host, String user, String password) 
+			throws Exception {
+		String url = "jdbc:mysql://" + host + ":3306/" + DB_NAME;
+		con = DriverManager.getConnection(url, user, password);
+	}
+
+	public static Database getInstance(String host, String user, String password)
+			throws Exception {
+		if (db == null) {
+			db = new Database(host, user, password);
+		}
+		return db;
+	}
+	
+	public void addRequestPermission(String recordId, String name, int type) throws Exception {
+		String sql = "INSERT INTO RequestPermission(RecordId, Name, Type) VALUES" +
+				"('" + recordId + "','" + name + "','" + type + "')";
+		con.createStatement().execute(sql);
+	}
+	
+	public void updateRequestRegisterationStatus(String recordId, String name, boolean registered) throws Exception {
+		String sql = "UPDATE RequestPermission SET Registered=" + registered;
+		con.createStatement().execute(sql);
+	}
+	
+	public void groupRecordIdAndParams(String recordId, String ownerName, String readName) throws Exception {
+		String sql = "INSERT INTO RecordPair(RecordId, OwnerName, ReadName) VALUES" +
+				"('" + recordId + "','" + ownerName + "','" + readName + "')";
+		con.createStatement().execute(sql);
+	}
+}
