c2ffbd91e4a7ff2eed23fa6153f1b33fa1e9950f
Byungchan An <ban@purdue.edu>
2015-03-23 20:01:53 -0400

    (1) include promise package (2) rough completion for retrieving public params

--- scenarios/health_care/apps/lab/routes/index.js
+++ scenarios/health_care/apps/lab/routes/index.js
@@ -1,20 +1,78 @@
-var request = require('request');
+var common = require('../common.js');
 
-/* GET home page. */
+/* GET: home page. */
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+/* request in promise format */
+var request = function(method, target) {
+    return new Promise(function(resolve, reject) {
+       if(method.toLowerCase() == 'post') {
+            require('request').post(target, function(error, response, body) {
+                if(error) reject(error);
+                else resolve({ "response": response, "body": body });
+            });
+       }
+       else if(method.toLowerCase() == 'get') {
+           require('request').get(target, function(error, response, body) {
+               if(error) reject(error); 
+               else resolve({ "response": response, "body": body });
+           });
+       }
+       else {
+           reject("Incorrect method!");
+       }
+    });
+};
+
+/* GET: get public params from claimsdefs for record id 
+ */
 exports.get_parameters = function(req, res) {
-    request.get('http://localhost:3001/param_names/' + req.params.id,
-        function(error, response, body) {
-            if(error) {
-                
-            }
-            else {
-               console.log(response); 
-               res.send(JSON.parse(body));
-               console.log(body);
-            }
-        });
+    var param_names = null;
+    var OwnerName = null;
+    var ReadName = null;
+
+    // 1. First, get names of claimdefs for the corresponding record id
+    request('get', 'http://localhost:3001/param_names/' + req.params.id)
+    .then(function(result) {
+        param_names = JSON.parse(result.body)[0];
+        OwnerName = param_names.OwnerName;
+        ReadName = param_names.ReadName;
+        console.log("OwnerName:" + OwnerName);
+        console.log("ReadName: " + ReadName);
+
+        // 2. Second, with this name, fetch claim defs and extract public defs
+        var queue = [];
+        queue.push(get_claim_and_extract_public_params(OwnerName));
+        queue.push(get_claim_and_extract_public_params(ReadName));
+        return common.promise.all(queue);
+    })
+    // 3. TODO
+    .then(function(result) {
+        console.log(result[0]);
+        console.log(result[1]);
+        //TODO
+        res.send("Still working!");
+    }, function(error) {
+        console.log("Errors: " + error);
+    });
 }
+
+var get_claim_and_extract_public_params = function(name) {
+    return request('get', 'http://localhost:3001/claimdef/' + name)
+        .then(function(result) {
+            console.log(result.body);
+            return extract_public_params_promise(result.body);
+        });
+};
+
+var extract_public_params_promise = function(ip) {
+    return new Promise(function(resolve, conflict) {
+        common.lab.extractPublicParams(ip, function(err, res) {
+            if(err) reject(err);
+            else resolve(res);
+        });
+    });
+};
+
