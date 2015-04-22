9b8d06adbee304199ef904e360b14094fff80f7d
Byungchan An <ban@purdue.edu>
2015-04-21 23:49:19 -0400

    more work on java codes for doctor

--- scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/consumer/Doctor.java
+++ scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/consumer/Doctor.java
@@ -1,11 +1,17 @@
 package org.ruchith.research.scenarios.healthcare.consumer;
 
+import java.io.FileInputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.cert.Certificate;
+
 import org.bouncycastle.util.encoders.Base64;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.node.ObjectNode;
 import org.ruchith.research.idm.user.Client;
-
+import org.ruchith.research.scenarios.healthcare.Util;
 
 /**
  * 
@@ -28,5 +34,17 @@ public class Doctor extends Client {
 		JsonNode params_node = mapper.readTree(params);
 		
 		return params_node.get("g").asText();		
+	}
+	
+	public String sessionSig(String content, String storePath, String storePass, String alias, String keyPass) throws Exception {
+		// Get user's private key
+		KeyStore ks = KeyStore.getInstance("JKS");
+		FileInputStream is = new FileInputStream(storePath);
+		ks.load(is, storePass.toCharArray());
+		PrivateKey key = (PrivateKey) ks.getKey(alias, keyPass.toCharArray());
+		//Certificate cert = (Certificate) ks.getCertificate(alias);
+		
+		String sessionSig = Util.createB64Sig(content, key);
+		return sessionSig;
 	}
 }
