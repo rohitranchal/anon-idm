6ea35d88f5f5f370ca741249c7d6aa46c73bfda1
Byungchan An <ban@purdue.edu>
2015-04-21 23:50:00 -0400

    more work on java code of lab

--- scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/lab/Lab.java
+++ scenarios/health_care/java/src/main/java/org/ruchith/research/scenarios/healthcare/lab/Lab.java
@@ -1,9 +1,19 @@
 package org.ruchith.research.scenarios.healthcare.lab;
 
+import java.io.ByteArrayInputStream;
+import java.security.MessageDigest;
+import java.security.Signature;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Iterator;
+
 import org.bouncycastle.util.encoders.Base64;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
+import org.ruchith.ae.base.AEParameters;
+import org.ruchith.research.idm.IdentityClaimDefinition;
 import org.ruchith.research.idm.idp.Configuration;
 import org.ruchith.research.scenarios.healthcare.lab.db.Database;
 
@@ -56,6 +66,67 @@ public class Lab {
 		}
 
 		return params;
+	}
+	
+	public boolean verifyClaimdef(String fetchedCert, String claimdef) {
+		try {
+			CertificateFactory factory = CertificateFactory.getInstance("X.509");
+			Certificate idpCert = factory.generateCertificate(new ByteArrayInputStream(fetchedCert.getBytes()));
+
+			MessageDigest dgst = MessageDigest.getInstance("SHA-512");
+			Signature sig = Signature.getInstance("SHA512withRSA");
+
+			System.out.println(claimdef);
+			ObjectMapper mapper = new ObjectMapper();
+			ObjectNode rootOn = (ObjectNode) mapper.readTree(claimdef);
+
+			String name = rootOn.get("name").asText();
+			String params = rootOn.get("params").asText();
+			String b64Dgst = rootOn.get("dgst").asText();
+			String b64Sig = rootOn.get("sig").asText();
+			String b64Cert = rootOn.get("cert").asText();
+			// String createDate = node.get("DateCreated").asText();
+
+			ObjectNode on = (ObjectNode) mapper.readTree(Base64.decode(params));
+			AEParameters aeParams = new AEParameters(on);
+
+			IdentityClaimDefinition tmpDef = new IdentityClaimDefinition(name, aeParams);
+
+			// Verify dgst
+			byte[] contentBytes = tmpDef.getDgstContet().getBytes();
+
+			dgst.reset();
+			dgst.update(contentBytes);
+			String newDgstVal = new String(Base64.encode(dgst.digest()));
+
+			if (!newDgstVal.equals(b64Dgst)) {
+				// TODO : Log error
+				return false;
+			}
+
+			// Verify signature
+			sig.initVerify(idpCert);
+			sig.update(contentBytes);
+			if (!sig.verify(Base64.decode(b64Sig))) {
+				// TODO : Log error
+				return false;
+			}
+
+			tmpDef.setB64Hash(b64Dgst);
+			tmpDef.setB64Sig(b64Sig);
+
+			ByteArrayInputStream bais = new ByteArrayInputStream(Base64.decode(b64Cert));
+			tmpDef.setCert(CertificateFactory.getInstance("X.509").generateCertificate(bais));
+	
+		} catch (Exception e) {
+			// Swallow for now
+			// TODO
+			e.printStackTrace();
+			return false;
+		}
+			
+		// Everything is good
+		return true;
 	}
 	
 	/**
